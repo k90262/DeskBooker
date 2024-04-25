86a45e52948a2d0bda92fb3875c15cb245b59e8f
Bill Ho <ycho.bill.lab@gmail.com>
2024-04-26 00:01:10 +0800

    [Testing and Implementing Business Logic] Test: Save a Desk Booking

--- DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
+++ DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
@@ -1,4 +1,5 @@
 using DeskBooker.Core.Domain;
+using Moq;
 
 namespace DeskBooker.Core.Processor;
 
@@ -41,4 +42,48 @@ public class DeskBookingRequestProcessorTests
 
         Assert.Equal("request", exception.ParamName);
     }
+
+    [Fact]
+    public void ShouldSaveDeskBooking()
+    {
+        // Arrange
+        DeskBooking savedDeskBooking = null;
+        Mock<IDeskBookingRepository> deskBookingRepositoryMock = new Mock<IDeskBookingRepository>();
+        deskBookingRepositoryMock.Setup(x => x.Save(It.IsAny<DeskBooking>()))
+            .Callback<DeskBooking>(deskBooking =>
+            {
+                savedDeskBooking = deskBooking;
+            });
+        var request = new DeskBookingRequest
+        {
+            FirstName = "Bill",
+            LastName = "Ho",
+            Email = "bill.ho@test.org",
+            Date = new DateTime(2024, 04, 20)
+        };
+        
+        // Act
+        DeskBookingResult result = _processor.BookDesk(request);
+        
+        // Assert
+        deskBookingRepositoryMock.Verify(x => x.Save(It.IsAny<DeskBooking>()), Times.Once);
+        Assert.NotNull(savedDeskBooking);
+        Assert.Equal(request.FirstName, savedDeskBooking.FirstName);
+        Assert.Equal(request.LastName, savedDeskBooking.LastName);
+        Assert.Equal(request.Date, savedDeskBooking.Date);
+        Assert.Equal(request.Email, savedDeskBooking.Email);
+    }
+}
+
+public interface IDeskBookingRepository
+{
+    void Save(DeskBooking deskBooking);
+}
+
+public class DeskBooking
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public DateTime Date { get; set; }
 }
