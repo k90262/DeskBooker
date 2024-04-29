afe074934cc2670bd0c85d3271ce6b772a7bf1e4
Bill Ho <ycho.bill.lab@gmail.com>
2024-04-29 23:01:41 +0800

    [Testing and Implementing Business Logic] Refactor: Store the Desk ID on the Booking

--- DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
+++ DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
@@ -22,9 +22,8 @@ public class DeskBookingRequestProcessor
         }
 
         var availableDesks = _deskRepository.GetAvailableDesks(request.Date);
-        if (availableDesks.Count() > 0)
+        if (availableDesks.FirstOrDefault() is Desk availableDesk)
         {
-            var availableDesk = availableDesks.First();
             var deskBooking = Create<DeskBooking>(request);
             deskBooking.DeskId = availableDesk.Id;
                 
