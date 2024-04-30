96df91c610b34591962e11033809889063e43a48
Bill Ho <ycho.bill.lab@gmail.com>
2024-04-30 22:18:11 +0800

    [Testing and Implementing Business Logic] Implement & Refactor: Return Success or NoDeskAvailable Result Code

--- DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
+++ DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
@@ -21,6 +21,8 @@ public class DeskBookingRequestProcessor
             throw new ArgumentNullException(nameof(request));
         }
 
+        var result = Create<DeskBookingResult>(request);
+        
         var availableDesks = _deskRepository.GetAvailableDesks(request.Date);
         if (availableDesks.FirstOrDefault() is Desk availableDesk)
         {
@@ -28,9 +30,15 @@ public class DeskBookingRequestProcessor
             deskBooking.DeskId = availableDesk.Id;
                 
             _deskBookingRepository.Save(deskBooking);
+
+            result.Code = DeskBookingResultCode.Success;
+        }
+        else
+        {
+            result.Code = DeskBookingResultCode.NoDeskAvailable;
         }
         
-        return Create<DeskBookingResult>(request);
+        return result;
     }
 
     private static T Create<T>(DeskBookingRequest request) where T : DeskBookingBase, new()
