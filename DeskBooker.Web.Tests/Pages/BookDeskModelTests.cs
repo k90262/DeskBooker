7fcd4c1ebf547df876de02f3060e35604384992b
Bill Ho <ycho.bill.lab@gmail.com>
2024-05-07 01:00:31 +0800

    [Adding Features in an ASP.NET Core App] Test: Redirect to the BookDeskConfirmation Page

--- DeskBooker.Web.Tests/Pages/BookDeskModelTests.cs
+++ DeskBooker.Web.Tests/Pages/BookDeskModelTests.cs
@@ -105,5 +105,34 @@ namespace DeskBooker.Web.Pages
       // Assert
       Assert.IsType(expectedActionRsultType, actionResult);
     }
+
+    [Fact]
+    public void ShouldRedirectToBookDeskConfirmationPage()
+    {
+      // Arrange
+      _deskBookingResult.Code = DeskBookingResultCode.Success;
+      _deskBookingResult.DeskBookingId = 7;
+      _deskBookingResult.FirstName = "Thomas";
+      _deskBookingResult.Date = new DateTime(2020, 1, 28);
+      
+      // Act
+      IActionResult actionResult = _bookDeskModel.OnPost();
+      
+      // Assert
+      var redirectToPageResult = Assert.IsType<RedirectToPageResult>(actionResult);
+      Assert.Equal("BookDeskConfirmation", redirectToPageResult.PageName);
+
+      IDictionary<string, object> routeValues = redirectToPageResult.RouteValues;
+      Assert.Equal(3, routeValues.Count);
+
+      var deskBookingId = Assert.Contains("DeskBookingId", routeValues);
+      Assert.Equal(_deskBookingResult.DeskBookingId, deskBookingId);
+      
+      var firstName = Assert.Contains("FirstName", routeValues);
+      Assert.Equal(_deskBookingResult.FirstName, firstName);
+      
+      var date = Assert.Contains("Date", routeValues);
+      Assert.Equal(_deskBookingResult.Date, date);
+    }
   }
 }
